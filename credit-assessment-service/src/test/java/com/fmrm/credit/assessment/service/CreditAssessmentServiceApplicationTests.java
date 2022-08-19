2c7def62c9870d49324a3fa39020bd1d3c712280
Fábio Monteiro <fabio.monteiro.21@hotmail.com>
2022-08-19 09:54:14 -0300

    Credit Assessment Service - clean

---
+++ credit-assessment-service/src/test/java/com/fmrm/credit/assessment/service/CreditAssessmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmrm.credit.assessment.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAssessmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
